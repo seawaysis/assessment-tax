0163a7c8b580277996185353b84b7711962f1c82
seawaysia <seawaysia@gmail.com>
2024-04-22 22:22:54 +0700

    create calculate story-1

--- main.go
+++ main.go
@@ -14,7 +14,12 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-type Tax struct {
+//	type levelTax []struct {
+//		min  float32
+//		max  float32
+//		rate int
+//	}
+type income struct {
 	TotalIncome float32      `json:"totalIncome"`
 	Wht         float32      `json:"wht"`
 	Allowances  *[]allowance `json:"allowances"`
@@ -22,6 +27,9 @@ type Tax struct {
 type allowance struct {
 	AllowanceType string  `json:"allowanceType"`
 	Amount        float32 `json:"amount"`
+}
+type tax struct {
+	Tax float32 `json:"tax"`
 }
 
 func main() {
@@ -67,11 +75,36 @@ func somemiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 	}
 }
 func calculations(c echo.Context) error {
-	t := new(Tax)
-	err := c.Bind(&t)
+	inc := new(income)
+	err := c.Bind(&inc)
 	if err != nil {
 		return c.JSON(http.StatusBadGateway, err)
 	}
+	l := []struct {
+		min  float32
+		max  float32
+		rate int
+	}{
+		{min: 1, max: 150000, rate: 0},
+		{min: 150001, max: 500000, rate: 10},
+		{min: 500001, max: 1000000, rate: 15},
+		{min: 1000001, max: 2000000, rate: 20},
+		{min: 2000001, max: 10000000000, rate: 35},
+	}
+
+	t := &tax{Tax: 0.0}
+	numTax := inc.TotalIncome - 60000.0
+	for _, v := range l {
+		if numTax >= v.min {
+			temp := (numTax - (v.min - 1))
+			if temp > v.max {
+				temp = v.max
+			}
+			t.Tax = t.Tax + (temp * float32(v.rate) / 100)
+			fmt.Printf("%.1f | %.1f | %.1f\n", numTax, temp, t.Tax)
+		}
+	}
+
 	return c.JSON(http.StatusOK, t)
 	//return c.String(http.StatusOK, c.Param("totalIncome"))
 }
---
+++ main_test.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestMakehttp(t *testing.T) {
+	t.Run("Happy server response", func(t *testing.T) {
+
+	})
+
+}
